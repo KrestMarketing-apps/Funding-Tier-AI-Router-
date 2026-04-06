bbda34dfadc1b7748eebe1242d572387ea503794
KrestMarketing-apps <vendors@krestmarketing.com>
2026-04-06 09:36:59 -0700

    Update route.js

--- api/test-env/route.js
+++ api/test-env/route.js
@@ -1,1 +1,10 @@
-
+export async function GET() {
+  return Response.json({
+    ok: true,
+    hasGhlApiKey: !!process.env.GHL_API_KEY,
+    hasGhlLocationId: !!process.env.GHL_LOCATION_ID,
+    hasRouterSecret: !!process.env.ROUTER_SHARED_SECRET,
+    hasBlobToken: !!process.env.BLOB_READ_WRITE_TOKEN,
+    appUrl: process.env.NEXT_PUBLIC_APP_URL || null
+  });
+}
