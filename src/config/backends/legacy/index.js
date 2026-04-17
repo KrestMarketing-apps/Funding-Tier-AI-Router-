61842a3bdb1859394071a5d181a867ef5b3ba9b6
KrestMarketing-apps <vendors@krestmarketing.com>
2026-04-16 21:21:37 -0700

    Update index.js

--- src/config/backends/legacy/index.js
+++ src/config/backends/legacy/index.js
@@ -1,1 +1,21 @@
-import { legacyBackend } from "@/config/backends/legacy";
+import { qualification } from "./qualification";
+import { routing } from "./routing";
+import { scoring } from "./scoring";
+import { compliance } from "./compliance";
+import { commission } from "./commission";
+import { objections } from "./objections";
+import { crmActions } from "./crmActions";
+
+export const legacyBackend = {
+  id: "legacy_capital_services",
+  name: "Legacy Capital Services",
+  type: "attorney_model_creditor_resolution",
+
+  qualification,
+  routing,
+  scoring,
+  compliance,
+  commission,
+  objections,
+  crmActions
+};
